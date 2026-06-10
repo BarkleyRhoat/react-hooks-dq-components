da39df623fe8f3d822621b34b9fc8db367ddcca6
BarkleyRhoat <barkleyrhoat@gmail.com>
2026-06-10 08:46:24 -0400

    added image

--- src/data/albums.js
+++ src/data/albums.js
@@ -1,57 +1,58 @@
 const albums = [
-  {
-    id: 1,
-    name: "Classic Tuna Cans Being Opened",
-    image:
-      "https://target.scene7.com/is/image/Target/GUEST_360eeed2-1747-4b15-8394-a22c7cd12230?wid=488&hei=488&fmt=pjpeg",
-    inLibrary: false,
-    genre: "Cats",
-  },
-  {
-    id: 2,
-    name: "Delicious Birds",
-    image: "https://i.redd.it/6wq3pboygwg31.jpg",
-    inLibrary: true,
-    genre: "Cats",
-  },
-  {
-    id: 3,
-    name: "Birds Are Friends, Not Food",
-    image:
-      "https://www.thoughtco.com/thmb/ycJrhH6sAuWzS756yJXwBYKQiOE=/1414x1414/smart/filters:no_upscale()/GettyImages-531312698-0f7bfd887e6e4902abab0bf8475312e3.jpg",
-    inLibrary: true,
-    genre: "Birds",
-  },
-  {
-    id: 4,
-    name: "Scary Music for Cats",
-    image: "https://img.zergnet.com/2564012_300.jpg",
-    inLibrary: true,
-    genre: "Cats",
-  },
-  {
-    id: 5,
-    name: "Soothing Sleepytime Sounds",
-    image:
-      "https://cdn.shopify.com/s/files/1/1814/0429/products/shakers-maracas-rattles-wooden-maracas-1263833251881_580x.jpg?v=1575933159",
-    inLibrary: false,
-    genre: "Snakes",
-  },
-  {
-    id: 6,
-    name: "Pet Sounds",
-    image: "https://upload.wikimedia.org/wikipedia/en/b/bb/PetSoundsCover.jpg",
-    inLibrary: true,
-    genre: "Goats",
-  },
-  {
-    id: 7,
-    name: "Aloha K.K.",
-    image:
-      "https://vignette.wikia.nocookie.net/animalcrossing/images/5/5d/NH-Album_Cover-Aloha_K.K..png/revision/latest?cb=20200502231742",
-    inLibrary: true,
-    genre: "Dogs",
-  },
+	{
+		id: 1,
+		name: "Classic Tuna Cans Being Opened",
+		image:
+			"https://target.scene7.com/is/image/Target/GUEST_360eeed2-1747-4b15-8394-a22c7cd12230?wid=488&hei=488&fmt=pjpeg",
+		inLibrary: false,
+		genre: "Cats",
+	},
+	{
+		id: 2,
+		name: "Delicious Birds",
+		image:
+			"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.flyaviary.com%2Fwp-content%2Fuploads%2F2024%2F05%2Fdelicious_gamey_bird_meat.jpg&f=1&nofb=1&ipt=5dd2a6adb3eee92d210152adaed2da9dcbc19885481e8336f916bc7ba6ec8f3d",
+		inLibrary: true,
+		genre: "Cats",
+	},
+	{
+		id: 3,
+		name: "Birds Are Friends, Not Food",
+		image:
+			"https://www.thoughtco.com/thmb/ycJrhH6sAuWzS756yJXwBYKQiOE=/1414x1414/smart/filters:no_upscale()/GettyImages-531312698-0f7bfd887e6e4902abab0bf8475312e3.jpg",
+		inLibrary: true,
+		genre: "Birds",
+	},
+	{
+		id: 4,
+		name: "Scary Music for Cats",
+		image: "https://img.zergnet.com/2564012_300.jpg",
+		inLibrary: true,
+		genre: "Cats",
+	},
+	{
+		id: 5,
+		name: "Soothing Sleepytime Sounds",
+		image:
+			"https://cdn.shopify.com/s/files/1/1814/0429/products/shakers-maracas-rattles-wooden-maracas-1263833251881_580x.jpg?v=1575933159",
+		inLibrary: false,
+		genre: "Snakes",
+	},
+	{
+		id: 6,
+		name: "Pet Sounds",
+		image: "https://upload.wikimedia.org/wikipedia/en/b/bb/PetSoundsCover.jpg",
+		inLibrary: true,
+		genre: "Goats",
+	},
+	{
+		id: 7,
+		name: "Aloha K.K.",
+		image:
+			"https://vignette.wikia.nocookie.net/animalcrossing/images/5/5d/NH-Album_Cover-Aloha_K.K..png/revision/latest?cb=20200502231742",
+		inLibrary: true,
+		genre: "Dogs",
+	},
 ];
 
 export { albums };
